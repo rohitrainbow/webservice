a9d6d9123f88c4bb10215af1fe7e67f8a88d0007
rohitrainbow <rohitrainbow@yahoo.co.in>
2019-04-10 09:01:28 +0530

    Frontend copied.

--- src/main/java/com/theimitation/webservice/controller/ProductController.java
+++ src/main/java/com/theimitation/webservice/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.theimitation.webservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class ProductController {
     }
 
     @GetMapping(value = { "", "/" })
+    @CrossOrigin(origins = "http://localhost:4200")
     public @NotNull Iterable<Product> getProducts() {
         return productService.getAllProducts();
     }
