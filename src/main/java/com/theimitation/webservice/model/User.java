865f9286366a18412f67af57648bb9a1fe4ea9b2
rohit <rohit@rohit-PC>
2019-04-15 00:34:35 -0700

    User Model Added

---
+++ src/main/java/com/theimitation/webservice/model/User.java
@@ -0,0 +1,49 @@
+package com.theimitation.webservice.model;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "username is required.")
+    @Basic(optional = false)
+    private String userName;
+	
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
---
+++ src/main/java/com/theimitation/webservice/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package com.theimitation.webservice.repository;
+
+public interface UserRepository {
+
+}
